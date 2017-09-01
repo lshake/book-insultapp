c0d579524b4ea7dfef13c25deb8e4f4da044b974
Lee Shakespeare <leeshakespeare@hsbc.com>
2017-09-01 15:04:04 +0100

    Updated output to trigger build.

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -30,7 +30,7 @@ public class InsultGenerator {
      if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
       article = "a";
      }
-     theInsult =  String.format("Thou ist %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
+     theInsult =  String.format("Thou art a %s %s %s %s!", article, rs.getString("first"), rs.getString("second"), rs.getString("noun"));
     }
     rs.close();
     connection.close();
